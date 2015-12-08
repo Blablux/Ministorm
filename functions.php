d4993b42079cc1402e9b4bd7c941773d575d7c6b
Blablux <hetrube@gmail.com>
2015-12-08 08:50:24 +0100

    updating the script to remove emojis

--- functions.php
+++ functions.php
@@ -1,5 +1,25 @@
 <?php
 
+function disable_emojis() {
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	remove_action( 'admin_print_styles', 'print_emoji_styles' );
+	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+	add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+}
+add_action( 'init', 'disable_emojis' );
+
+function disable_emojis_tinymce( $plugins ) {
+	if ( is_array( $plugins ) ) {
+		return array_diff( $plugins, array( 'wpemoji' ) );
+	} else {
+		return array();
+	}
+}
+
 function theme_enqueue_styles() {
   wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
   wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/editor-style.css' );
@@ -8,8 +28,6 @@ function theme_enqueue_styles() {
 }
 
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-remove_action( ‘wp_head’, ‘print_emoji_detection_script’, 7 );
-remove_action( ‘wp_print_styles’, ‘print_emoji_styles’ );
 
 /**
  * Prints HTML with meta information for the current post-date/time and author.
