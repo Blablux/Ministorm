d90d15ae95070069808dc7dda66c82cf20b1e617
Blablux <hetrube@gmail.com>
2016-04-24 23:38:06 +0200

    corrections

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -65,43 +65,26 @@ if ( ! function_exists( 'minnow_posted_on' ) ) :
 /**
  * Prints HTML with meta information for the current post-date/time and author.
  */
-function minnow_posted_on() {
-	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
-	}
-
-	$time_string = sprintf( $time_string,
-		esc_attr( get_the_date( 'c' ) ),
-		esc_html( get_the_date() ),
-		esc_attr( get_the_modified_date( 'c' ) ),
-		esc_html( get_the_modified_date() )
-	);
-
-	$posted_on = '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>';
-
-	$byline = '<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>';
-
-	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>';
-
-	if ( 'post' == get_post_type() ) {
-
-		/* translators: used between list items, there is a space after the comma */
-		$tags_list = get_the_tag_list( '', __( ', ', 'minnow' ) );
-		if ( $tags_list ) {
-			echo '<span class="tags-links">' . $tags_list . '</span>';
-		}
-	}
-
-	if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
-		echo '<span class="comments-link">';
-		comments_popup_link( __( 'Leave a comment', 'minnow' ), __( '1 Comment', 'minnow' ), __( '% Comments', 'minnow' ) );
-		echo '</span>';
-	}
-
-	edit_post_link( __( 'Edit', 'minnow' ), '<span class="edit-link">', '</span>' );
-
-}
+ function minnow_posted_on() {
+ 	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+ 	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+ 		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+ 	}
+ 	$time_string = sprintf( $time_string,
+ 		esc_attr( get_the_date( 'c' ) ),
+ 		esc_html( get_the_date() ),
+ 		esc_attr( get_the_modified_date( 'c' ) ),
+ 		esc_html( get_the_modified_date() )
+ 	);
+ 	$posted_on = '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>';
+ 	echo '<span class="posted-on">' . $posted_on . '</span>';
+ 	if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+ 		echo '<span class="comments-link">';
+ 		comments_popup_link( __( 'Leave a comment', 'minnow' ), __( '1 Comment', 'minnow' ), __( '% Comments', 'minnow' ) );
+ 		echo '</span>';
+ 	}
+ 	edit_post_link( __( 'Edit', 'minnow' ), '<span class="edit-link">', '</span>' );
+ }
 endif;
 
 if ( ! function_exists( 'minnow_entry_format' ) ) :
